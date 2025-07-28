f42577d0b125c359e0ef2a47b2a2dbfe47b96a54
Holymichael99 <omofolarin1994@gmail.com>
2025-07-28 09:10:13 +0100

    Load csv file

---
+++ weather.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas.plotting import autocorrelation_plot
+
+# Load the CSV file
+file_path = 'file.csv'
+df = pd.read_csv(file_path, encoding='ascii')

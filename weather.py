cfe66bd5c2105f1685cf40df9d0e98cce56acb08
Holymichael99 <omofolarin1994@gmail.com>
2025-07-28 09:16:50 +0100

    plot temperature over time

--- weather.py
+++ weather.py
@@ -15,3 +15,11 @@ df['Date/Time'] = pd.to_datetime(df['Date/Time'])
 
 # Set 'Date/Time' as index
 df.set_index('Date/Time', inplace=True)
+
+# Plot temperature over time
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=df, x=df.index, y='Temp_C')
+plt.title('Temperature Over Time')
+plt.xlabel('Time')
+plt.ylabel('Temperature (C)')
+plt.show()
